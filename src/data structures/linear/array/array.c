8f73912f322f139c7fbac6db613704d2bd9b6167
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-03-31 15:28:51 +0200

    Added example for an one-dimensional array

---
+++ src/data structures/linear/array/array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // One-dimensional array.
+    int array[5] = {1, 2, 3, 4, 5};                        // Allocate memory for five elements of type int and initialize each element with the corresponding value.
+    printf("Size: %llu\n", sizeof(array));                 // Print the size (in bytes) of the array.
+    printf("Length: %llu\n", sizeof(array) / sizeof(int)); // Print the length (number of elements) of the array.
+
+    for (unsigned int i = 0; i < sizeof(array) / sizeof(int); ++i) // Iterate through the array
+    {
+        printf("Index: %u, memory address: %p, value: %u\n", i, &array[i], array[i]); // Print each elements index, memory location and value.
+    }
+}
