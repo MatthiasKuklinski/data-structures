a6dc5d031b898de9488d3831fd346515e573c1d1
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-03-31 15:53:41 +0200

    Added type modifiers

--- src/data structures/linear/array/one-dimensional.c
+++ src/data structures/linear/array/one-dimensional.c
@@ -4,9 +4,9 @@ int main()
 {
     unsigned int array[3] = {1, 2, 3};               // Allocate memory for five elements of type int and initialize each element with the corresponding value.
     printf("Size: %llu\n", sizeof(array));                 // Print the size (in bytes) of the array.
-    printf("Length: %llu\n", sizeof(array) / sizeof(int)); // Print the length (number of elements) of the array.
+    printf("Length: %llu\n", sizeof(array) / sizeof(unsigned int)); // Print the length (number of elements) of the array.
 
-    for (unsigned int i = 0; i < sizeof(array) / sizeof(int); ++i) // Iterate through the array
+    for (unsigned int i = 0; i < sizeof(array) / sizeof(unsigned int); ++i) // Iterate through the array
     {
         printf("Index: %u, memory address: %p, value: %u\n", i, &array[i], array[i]); // Print each elements index, memory location and value.
     }
