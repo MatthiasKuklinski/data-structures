3f8003ddaf7c1abb80efd5bed147c140cfbedac5
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-01 19:44:44 +0200

    Added library to free the memory

--- src/data structures/linear/array/dynamic/one-dimensional.c
+++ src/data structures/linear/array/dynamic/one-dimensional.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     int i;
     int *ptr;
 
-    ptr = calloc(3, sizeof(int));
+    ptr = calloc(3, sizeof(int)); // Reserve memory for the array.
+    printf("%p", ptr);
+
     free(ptr);
 }
