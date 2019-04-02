1ab454178f781fc713a22cf9b9b898ad29593e17
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-02 18:11:32 +0200

    Added array population and print

--- src/data structures/linear/array/dynamic/one-dimensional.c
+++ src/data structures/linear/array/dynamic/one-dimensional.c
@@ -3,9 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    int *ptr = malloc(argv[1], sizeof(int)); // Reserve memory (size is specified by the user) for the array.
-    for (int i = 0; i < 3; i++)              // Iterate through the array.
+    long length = strtol(argv[1], NULL, 10); // Convert char/string into long
+    int *ptr = malloc(length * sizeof(int)); // Reserve memory (size is specified by the user) for the array.
+    for (int i = 0; i < length; i++)         // Iterate through the array.
         *(ptr + i) = i;                      // Initialize each index with the corresponding value.
+
+    for (int i = 0; i < length; i++)
+        printf("%d", *(ptr + i));
 
     free(ptr);
 }
