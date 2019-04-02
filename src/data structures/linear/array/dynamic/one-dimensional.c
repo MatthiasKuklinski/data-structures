788135212a9b73ae787e19761738727db3d806ab
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-02 18:16:00 +0200

    Added type modifier

--- src/data structures/linear/array/dynamic/one-dimensional.c
+++ src/data structures/linear/array/dynamic/one-dimensional.c
@@ -3,10 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-    long length = strtol(argv[1], NULL, 10); // Convert char/string into long.
-    int *ptr = malloc(length * sizeof(int)); // Reserve memory (size is specified by the user at runtime) for the array.
-    for (int i = 0; i < length; i++)         // Iterate through the array.
-        *(ptr + i) = i;                      // Initialize each index with the corresponding value.
+    const long length = strtol(argv[1], NULL, 10); // Convert char/string into long.
+    int *ptr = malloc(length * sizeof(int));       // Reserve memory (size is specified by the user at runtime) for the array.
+    for (int i = 0; i < length; i++)               // Iterate through the array.
+        *(ptr + i) = i;                            // Initialize each index with the corresponding value.
 
     for (int i = 0; i < length; i++) // Iterate through the array.
         printf("%d\n", *(ptr + i));  // Print the value of each index.
