e328146d9902d451a0b21cb04d6af61524b4cde0
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-02 18:12:57 +0200

    Added comments

--- src/data structures/linear/array/dynamic/one-dimensional.c
+++ src/data structures/linear/array/dynamic/one-dimensional.c
@@ -3,13 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    long length = strtol(argv[1], NULL, 10); // Convert char/string into long
-    int *ptr = malloc(length * sizeof(int)); // Reserve memory (size is specified by the user) for the array.
+    long length = strtol(argv[1], NULL, 10); // Convert char/string into long.
+    int *ptr = malloc(length * sizeof(int)); // Reserve memory (size is specified by the user at runtime) for the array.
     for (int i = 0; i < length; i++)         // Iterate through the array.
         *(ptr + i) = i;                      // Initialize each index with the corresponding value.
 
-    for (int i = 0; i < length; i++)
-        printf("%d", *(ptr + i));
+    for (int i = 0; i < length; i++) // Iterate through the array.
+        printf("%d", *(ptr + i));    // Print the value of each index.
 
-    free(ptr);
+    free(ptr); // Free the allocated memory.
 }
