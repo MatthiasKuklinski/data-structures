96729b60c7cdbc605defb094c3fc7ab9e103f220
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-04 10:28:28 +0200

    Added comments

--- src/data structures/linear/dynamic/linked list/singly-linked-list.c
+++ src/data structures/linear/dynamic/linked list/singly-linked-list.c
@@ -17,24 +17,25 @@ int main(int argc, char *argv[])
 
 void list_print(node_t *head)
 {
-    node_t *current = head; // Set the current node
+    node_t *current = head; // Point the current node to the head of the list.
 
-    while (current != NULL)
+    while (current != NULL) // Iterate through the sequence until the last node is reached.
     {
-        printf("%d\n", current->data);
-        current = current->next;
+        printf("%d\n", current->data); // Print the value of the current node.
+        current = current->next;       // Point the current node to the next node.
     }
 }
 
 void list_push(node_t *head, int data)
 {
-    node_t *current = head;
-    while (current->next != NULL)
+    node_t *current = head; // Point the current node to the head of the list.
+
+    while (current->next != NULL) // Iterate through the sequence until the last node is reached.
     {
-        current = current->next;
+        current = current->next; // Point the current node to the next node.
     }
 
-    current->next = malloc(sizeof(node_t));
-    current->next->data = data;
-    current->next->next = NULL;
+    current->next = malloc(sizeof(node_t)); // Allocate space for the new node and point the latest node to it.
+    current->next->data = data;             // Populate the added node with the passed in data argument.
+    current->next->next = NULL;             // Declare the added node to be the end of the list.
 }
