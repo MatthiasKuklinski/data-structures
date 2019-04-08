9d8b1b4c896c4de17c859833b35036a618558874
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-08 19:20:06 +0200

    Updated names and added pop declarations

--- src/data structures/linear/dynamic/linked list/singly linked list/include/singly-linked-list.h
+++ src/data structures/linear/dynamic/linked list/singly linked list/include/singly-linked-list.h
@@ -1,20 +1,22 @@
 #pragma once
-
-typedef struct node
-{
-    int value;
-    struct node *next;
-} node_t;
+#include "singly-linked-list-node.h"
 
 typedef void (*sll_callback)(node_t *node);
 
-node_t *sll_create(const int value, node_t *successor);
-void sll_traverse(node_t *node, const sll_callback cb);
-void sll_free(node_t *node);
-void sll_delete(node_t *node, const unsigned int index);
-int sll_length(node_t *node);
+node_t *sll_construct(const int value, const node_t *successor);
+void sll_destruct(node_t *node);
+
 void sll_prepend(node_t *node, const int value);
 void sll_append(node_t *node, const int value);
 void sll_insert(node_t *node, const int value, const unsigned int index);
+
+void sll_pop(node_t *node, const unsigned int index);
+void sll_pop_first(node_t *node);
+void sll_pop_last(node_t *node);
+
+node_t *sll_get(node_t *node, const unsigned int index);
+void sll_set(node_t *node, const unsigned int index, const int value);
+
+void sll_traverse(node_t *node, const sll_callback cb);
+int sll_length(node_t *node);
 void sll_print(node_t *node);
-node_t *sll_get(node_t *node, const int index);
