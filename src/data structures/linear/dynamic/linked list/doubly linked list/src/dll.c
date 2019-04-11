1ff6165ce87d910c0674df38cdb2de8e3bcb9ffd
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-11 20:12:35 +0200

    Fixed pointer

--- src/data structures/linear/dynamic/linked list/doubly linked list/src/dll.c
+++ src/data structures/linear/dynamic/linked list/doubly linked list/src/dll.c
@@ -29,6 +29,7 @@ void dll_prepend(dll_node_t *node, const int value)
         return;
 
     node->successor = dll_node(node->value, node, node->successor); // Create a copy of the node and assign the provided value ("move" the node to the second position).
+    node->successor->successor->predecessor = node->successor;
     node->value = value;
 }
 
