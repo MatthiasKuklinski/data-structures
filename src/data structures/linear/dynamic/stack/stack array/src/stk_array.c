7a09b58ef01a23702a2b4bd1ec74bfc267ed968d
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-13 17:42:51 +0200

    Implemented constructor

---
+++ src/data structures/linear/dynamic/stack/stack array/src/stk_array.c
@@ -0,0 +1,13 @@
+#include <stk_array.h>
+
+#include <stdlib.h>
+
+stack_t *stack(unsigned int capacity)
+{
+    stack_t *stack = malloc(sizeof(stack_t));             // Allocate memory.
+    stack->capacity = capacity;                           // Set capacity.
+    stack->top = -1;                                      // Set the top to < 0 (empty).
+    stack->array = malloc(stack->capacity * sizeof(int)); // Allocate memory for the related array.
+
+    return stack;
+}
