be1dceb2ce2c60ea9a55fd551e60ff94f10b6a8f
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-15 16:58:29 +0200

    Renamed array into items

--- src/data structures/linear/dynamic/stack/stack array/include/stk_ary.h
+++ src/data structures/linear/dynamic/stack/stack array/include/stk_ary.h
@@ -4,9 +4,9 @@ typedef struct stk_ary
 {
     int top;
     unsigned int capacity;
-    int *array;
+    int *items;
 } stk_ary_t;
 
 stk_ary_t *stk_ary(unsigned int capacity);
-void stk_ary_push(stk_ary_t *stk_ary, int value);
+void stk_ary_push(stk_ary_t *stk_ary, int item);
 void stk_ary_pop(stk_ary_t **stk_ary);
--- src/data structures/linear/dynamic/stack/stack array/src/stk_ary.c
+++ src/data structures/linear/dynamic/stack/stack array/src/stk_ary.c
@@ -8,17 +8,17 @@ stk_ary_t *stk_ary(unsigned int capacity)
     stk_ary_t *stk_ary = malloc(sizeof(stk_ary_t));           // Allocate memory.
     stk_ary->capacity = capacity;                             // Set capacity.
     stk_ary->top = -1;                                        // Set the top to < 0 (empty).
-    stk_ary->array = malloc(stk_ary->capacity * sizeof(int)); // Allocate memory for the related array.
+    stk_ary->items = malloc(stk_ary->capacity * sizeof(int)); // Allocate memory for the related array.
 
     return stk_ary;
 }
 
-void stk_ary_push(stk_ary_t *stk_ary, int value)
+void stk_ary_push(stk_ary_t *stk_ary, int item)
 {
     if (!stk_ary)
         return;
 
-    stk_ary->array[++stk_ary->top] = value; // Increment top and append the requested value.
+    stk_ary->items[++stk_ary->top] = item; // Increment top and append the requested item.
 }
 
 void stk_ary_pop(stk_ary_t **stk_ary)
@@ -26,5 +26,5 @@ void stk_ary_pop(stk_ary_t **stk_ary)
     if (!stk_ary)
         return;
 
-    (*stk_ary)->array = realloc((*stk_ary)->array, (*stk_ary)->top-- * sizeof(int));
+    (*stk_ary)->items = realloc((*stk_ary)->items, (*stk_ary)->top-- * sizeof(int));
 }
