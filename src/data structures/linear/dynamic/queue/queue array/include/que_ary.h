92df17c7aa722a2782f5a653402b080a8329f8db
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-19 09:46:45 +0200

    Declared empty and full functions

--- src/data structures/linear/dynamic/queue/queue array/include/que_ary.h
+++ src/data structures/linear/dynamic/queue/queue array/include/que_ary.h
@@ -7,4 +7,7 @@ typedef struct que_ary
 } que_ary_t;
 
 que_ary_t *que_ary(unsigned int capacity);
+int que_ary_empty(que_ary_t *que_ary);
+int que_ary_full(que_ary_t *que_ary);
+// void que_ary_enqueue(que_ary_t *que_ary, int item);
 void que_ary_delete(que_ary_t **que_ary);
