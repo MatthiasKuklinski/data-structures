0f4e229e5e896ed4402f0d337ba415555cf7e3dc
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-19 14:49:03 +0200

    Updated print function

--- src/data structures/linear/dynamic/linked list/doubly linked list/main.c
+++ src/data structures/linear/dynamic/linked list/doubly linked list/main.c
@@ -3,17 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void dll_print(dll_node_t *node)
+void dll_print(dll_node_t *dll_node)
 {
-    printf("%-32d%-32p%-32p%-32p\n", node->value, node, node->predecessor, node->successor);
+    if (!dll_node)
+        return;
+
+    printf("%-32d%-32p%-32p%-32p\n", dll_node->value, dll_node, dll_node->predecessor, dll_node->successor);
 }
 
 void menu()
 {
     printf("- Doubly Linked List -\n");
-    printf("Print(p)\n");
-    printf("Get(g)\n");
-    printf("Set(s)\n");
+    printf("Print(q)\n");
     printf("Create(c)\n");
     printf("Prepend(b)\n");
     printf("Append(a)\n");
@@ -21,9 +22,11 @@ void menu()
     printf("Pop(r)\n");
     printf("Pop first(t)\n");
     printf("Pop last(z)\n");
+    printf("Get(g)\n");
+    printf("Set(s)\n");
     printf("Reverse(o)\n");
     printf("Length(l)\n");
-    printf("Destroy(d)\n");
+    printf("Delete(d)\n");
     printf("Menu(m)\n");
     printf("Exit(e)\n");
 }
@@ -36,37 +39,27 @@ void controller(const char cmd)
 
     switch (cmd)
     {
-    case 'p':
+    case 'q':
+        printf("%-32s%-32s%-32s%-32s\n", "Value", "Memory Address", "Predecessor Memory Address", "Successor Memory Address");
         dll_traverse(list, dll_print);
-        break;
-    case 'g':
-        printf("Index:");
-        scanf("%d", &index);
-        dll_node_t *temp_node = dll_get(list, index);
-        printf("%-32d%-32p%-32p%-32p\n", temp_node->value, temp_node, temp_node->predecessor, temp_node->successor);
-        break;
-    case 's':
-        printf("Index:");
-        scanf("%d", &index);
-        printf("Value:");
-        scanf("%d", &value);
-        dll_set(list, index, value);
         break;
     case 'c':
         printf("Value:");
         scanf("%d", &value);
-        dll_destroy(list);
+        dll_delete(list);
         list = dll_node(value, NULL, NULL);
         break;
     case 'b':
         printf("Value:");
         scanf("%d", &value);
         dll_prepend(list, value);
+        dll_traverse(list, dll_print);
         break;
     case 'a':
         printf("Value:");
         scanf("%d", &value);
         dll_append(list, value);
+        dll_traverse(list, dll_print);
         break;
     case 'i':
         printf("Index:");
@@ -86,6 +79,19 @@ void controller(const char cmd)
     case 'z':
         dll_pop_last(list);
         break;
+    case 'g':
+        printf("Index:");
+        scanf("%d", &index);
+        dll_node_t *temp_node = dll_get(list, index);
+        printf("%-32d%-32p%-32p%-32p\n", temp_node->value, temp_node, temp_node->predecessor, temp_node->successor);
+        break;
+    case 's':
+        printf("Index:");
+        scanf("%d", &index);
+        printf("Value:");
+        scanf("%d", &value);
+        dll_set(list, index, value);
+        break;
     case 'o':
         dll_reverse(&list);
         break;
@@ -93,13 +99,13 @@ void controller(const char cmd)
         printf("%lu\n", dll_length(list));
         break;
     case 'd':
-        dll_destroy(list);
+        dll_delete(list);
         break;
     case 'm':
         menu();
         break;
     case 'e':
-        dll_destroy(list);
+        dll_delete(list);
         exit(0);
     default:
         break;
