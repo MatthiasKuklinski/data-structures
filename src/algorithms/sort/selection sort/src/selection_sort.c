8d7718db7ee9e02ce0366026f4c3e973d23fac34
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-22 15:53:31 +0200

    Added comments

--- src/algorithms/sort/selection sort/src/selection_sort.c
+++ src/algorithms/sort/selection sort/src/selection_sort.c
@@ -5,19 +5,19 @@ void selection_sort(int ary[], const unsigned int n)
     int smallest_element_index, temp;
     for (unsigned int i = 0; i < n - 1; ++i)
     {
-        smallest_element_index = i;
+        smallest_element_index = i; // Set the smallest index to the current iteration index.
 
-        for (unsigned int j = i + 1; j < n; ++j)
+        for (unsigned int j = i + 1; j < n; ++j) // Iterate through each successing element.
         {
-            if (ary[j] < ary[smallest_element_index])
-                smallest_element_index = j;
+            if (ary[j] < ary[smallest_element_index]) // Check for smallest element.
+                smallest_element_index = j;           // Temporarily store the smallest index.
         }
 
-        if (smallest_element_index != i)
+        if (smallest_element_index != i) // Check if a smaller element was found.
         {
-            temp = ary[i];
-            ary[i] = ary[smallest_element_index];
-            ary[smallest_element_index] = temp;
+            temp = ary[i];                        // Store the current element temporarily.
+            ary[i] = ary[smallest_element_index]; // Assigne the smallest element to the current element.
+            ary[smallest_element_index] = temp;   // Swap the smallest element with the current element.
         }
     }
 }
