0041a6f796214f29aa9672670da8aacb8eea222a
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-23 19:07:07 +0200

    Added outer loop

---
+++ src/algorithms/sort/quick sort/src/quick_sort.c
@@ -0,0 +1,17 @@
+#include <quick_sort.h>
+
+void quick_sort(int ary[], const unsigned int m, const unsigned int n)
+{
+    if(m < n)
+    {
+        int pivot, i;
+        pivot = ary[n];
+        i = m-1;
+        for (unsigned int j = m; j < n-1; ++j)
+            if (ary[j] <= pivot)
+            {
+                
+            }
+        
+    }
+}
