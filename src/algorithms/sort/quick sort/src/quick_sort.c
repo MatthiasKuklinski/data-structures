15368ae56b73a0dcb60e954a1bdbf873d22e15ef
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-23 19:10:26 +0200

    Implemented swap functionality

--- src/algorithms/sort/quick sort/src/quick_sort.c
+++ src/algorithms/sort/quick sort/src/quick_sort.c
@@ -1,17 +1,21 @@
 #include <quick_sort.h>
 
+void swap(int *x, int *y)
+{
+    int temp = *x; // Store the value of pointer x temporarily.
+    *x = *y;       // Assign x to the value of pointer y.
+    *y = temp;     // Assign y to the temporarly stored former value of pointer x.
+}
+
 void quick_sort(int ary[], const unsigned int m, const unsigned int n)
 {
-    if(m < n)
+    if (m < n)
     {
         int pivot, i;
         pivot = ary[n];
-        i = m-1;
-        for (unsigned int j = m; j < n-1; ++j)
+        i = m - 1;
+        for (unsigned int j = m; j < n - 1; ++j)
             if (ary[j] <= pivot)
-            {
-                
-            }
-        
+                swap(&ary[i], &ary[j]);
     }
 }
