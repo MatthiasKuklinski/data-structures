6a2dee6e112bede41dda8258a8d9f4d6c69795c7
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-24 19:52:34 +0200

    Implemented test case

---
+++ src/algorithms/sort/quick sort/main.c
@@ -0,0 +1,15 @@
+#include <quick_sort.h>
+
+#include <stdio.h>
+
+#define LENGTH 12
+
+int main()
+{
+    int array[] = {7, 1, 326, 832, 225, 32323, 45, 1, 36, 43534, 34534, 2};
+    quick_sort(array, 0, LENGTH);
+        for (int i = 0; i < LENGTH; ++i)
+    {
+        printf("%d\n", array[i]);
+    }
+}
