42fe60b0356280cfaf15eefe7e513eae5b8acb5e
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-24 19:52:27 +0200

    Added comments

--- src/algorithms/sort/quick sort/src/quick_sort.c
+++ src/algorithms/sort/quick sort/src/quick_sort.c
@@ -7,15 +7,15 @@ void swap(int *x, int *y)
     *y = temp;     // Assign y to the temporarly stored former value of pointer x.
 }
 
-void quick_sort(int ary[], const unsigned int m, const unsigned int n)
+void partition(int ary[], const unsigned int m, const unsigned int n)
 {
     if (m < n)
     {
         int pivot, i;
-        pivot = ary[n];
-        i = m - 1;
+        pivot = ary[n]; // Set the pivot based on the parameter m (minimum).
+        i = m - 1;      // Set the index of the smaller element relative to the pivot.
         for (unsigned int j = m; j < n - 1; ++j)
-            if (ary[j] <= pivot)
-                swap(&ary[i], &ary[j]);
+            if (ary[j] <= pivot)          // Check if the current element is smaller or equal to the pivot.
+                swap(&ary[i++], &ary[j]); // Swap the elements and increment i.
     }
 }
