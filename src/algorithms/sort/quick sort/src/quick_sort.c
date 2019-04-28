0a2bef475c6a7a8622dd36e38ada40350e149ed9
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-28 15:00:26 +0200

    Renamed variables

--- src/algorithms/sort/quick sort/src/quick_sort.c
+++ src/algorithms/sort/quick sort/src/quick_sort.c
@@ -7,17 +7,17 @@ void swap(int *x, int *y)
     *y = temp;     // Assign y to the temporarly stored former value of pointer x.
 }
 
-void partition(int ary[], const unsigned int low, const unsigned int high)
+int partition(int ary[], const unsigned int lm, const unsigned int rm)
 {
-    if (low < high)
+    if (lm < rm) // Check if leftmark is smaller than rightmark.
     {
         int pivot, i;
-        pivot = ary[high]; // Set the pivot based on the parameter m (minimum).
-        i = low - 1;      // Set the index of the smaller element relative to the pivot.
-        for (unsigned int j = low; j < high; ++j)
+        pivot = ary[rm]; // Set the pivot based on the parameter rm (rightmark).
+        i = lm - 1;      // Set the index of the smallest element.
+        for (unsigned int j = lm; j < rm; ++j)
             if (ary[j] <= pivot)          // Check if the current element is smaller or equal to the pivot.
                 swap(&ary[i++], &ary[j]); // Swap the elements and increment i.
-        swap(&ary[i + 1], &ary[high - 1]);
+        swap(&ary[i + 1], &ary[rm - 1]);
         return ++i;
     }
 }
