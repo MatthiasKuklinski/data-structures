0c2be57906fdd6dd9a32933c7c5c7e0bf69594ef
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-04-28 15:00:41 +0200

    Adjusted return type

--- src/algorithms/sort/quick sort/include/quick_sort.h
+++ src/algorithms/sort/quick sort/include/quick_sort.h
@@ -1,4 +1,4 @@
 #pragma once
 
 void swap(int *x,int *y);
-void quick_sort(int ary[], const unsigned int low, const unsigned int high);
+int partition(int ary[], const unsigned int lm, const unsigned int rm);
