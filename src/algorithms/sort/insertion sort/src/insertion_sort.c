52c9b551ce84c0694f82f405d9358daad0e8b1b1
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-03-16 09:38:16 +0100

    Fix white space

--- src/algorithms/sort/insertion sort/src/insertion_sort.c
+++ src/algorithms/sort/insertion sort/src/insertion_sort.c
@@ -9,7 +9,7 @@ void insertion_sort(size_t ary[], const size_t n)
         j = i - 1;
         while(key < ary[j] && j >= 0)  // Iterate through the elements until the temporarily stored element(key) is larger than the element it is compared to or smaller than the first element.
         {
-            ary[j + 1] = ary[j];        // Move each element one index to the right.
+            ary[j + 1] = ary[j];       // Move each element one index to the right.
             j--;
         }
         ary[j + 1] = key;              // Place the temporarily stored element(key) into the sorted elements at the corresponding position.
