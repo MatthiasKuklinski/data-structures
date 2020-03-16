e7fb64c9667cfcac82daf1cbfb8cc87db73fc0cd
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-03-16 09:41:01 +0100

    Add comments

--- src/algorithms/sort/bubble sort/src/bubble_sort.c
+++ src/algorithms/sort/bubble sort/src/bubble_sort.c
@@ -1,21 +1,21 @@
 #include <bubble_sort.h>
 
-void bubble_sort(int ary[], int n)
+void bubble_sort(size_t ary[], const size_t n)
 {
     int temp, i, j;
-    int swapped = 1;
-    while (swapped)
+    unsigned int swapped = 1;
+    while (swapped) // Iterate through the elements until each element is sorted.
     {
-        swapped = 0;
+        swapped = 0; // Set the swapped flag to false. Assume the data structure is sorted before each inner iteration.
         for (i = 0; i < n - 1; ++i)
         {
             j = i + 1;
-            if (ary[i] > ary[j]) // Check if the current element is greater than its successing element.
+            if (ary[i] > ary[j]) // Check if the current element is greater than its successor.
             {
                 temp = ary[i];   // Store the current element temporarily.
-                ary[i] = ary[j]; // Assigne the successing element to the current element.
-                ary[j] = temp;   // Assigne the temporary element to the successing element.
-                swapped = 1;     // Set the swapped flag.
+                ary[i] = ary[j]; // Assigne the successor to the current element.
+                ary[j] = temp;   // Assigne the temporary element to the successor.
+                swapped = 1;     // Set the swapped flag to true(indicate that the data structure was not sorted).
             }
         }
     }
