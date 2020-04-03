0e1cbd69552072867a286312902dd463ed7b5ae9
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-04-03 10:19:00 +0200

    Replace int with size_t

--- src/algorithms/sort/quick sort/src/quick_sort.c
+++ src/algorithms/sort/quick sort/src/quick_sort.c
@@ -1,30 +1,29 @@
 #include <quick_sort.h>
 
-void swap(int *x, int *y)
+void swap(size_t *x, size_t *y)
 {
-    int temp = *x; // Store the value of pointer x temporarily.
+    size_t t = *x; // Store the value of pointer x temporarily.
     *x = *y;       // Assign x to the value of pointer y.
-    *y = temp;     // Assign y to the temporarly stored former value of pointer x.
+    *y = t;        // Assign y to the temporarly stored former value of pointer x(effectively swap them).
 }
 
-int partition(int ary[], const int lm, const int rm)
+size_t partition(size_t ary[], const size_t lm, const size_t rm)
 {
-    int pivot, i;
-    pivot = ary[rm];                   // Set the pivot based on the parameter rm (rightmark): Lomuto partition scheme.
-    i = lm - 1;                        // Set the tracking index based on the leftmark.
-    for (int j = lm; j <= rm - 1; ++j) // Iterate through the array.
-        if (ary[j] < pivot)            // Check if the current element is smaller than the pivot.
-            swap(&ary[++i], &ary[j]);  // Increment i and swap the elements.
-    swap(&ary[++i], &ary[rm]);         // Increment i and place the pivot at the correct position.
-    return i;
+    size_t pivot = ary[rm];           // Set the pivot based on the parameter rm (rightmark): Lomuto partition scheme.
+    size_t i = lm - 1;                // Set the tracking index based on the leftmark.
+    for (size_t j = lm; j < rm; j++)  // Iterate through the array.
+        if (ary[j] <= pivot)          // Check if the current element is smaller than the pivot.
+            swap(&ary[++i], &ary[j]); // Increment i and swap the elements.
+    swap(&ary[++i], &ary[rm]);        // Increment i and place the pivot at the correct position.
+    return i;                         // Return the index of the pivot element after it was placed at its correspondig position.
 }
 
-void quick_sort(int ary[], const int lm, const int rm)
+void quick_sort(size_t ary[], const size_t lm, const size_t rm)
 {
     if (lm < rm) // Check if the leftmark is smaller than the rightmark.
     {
-        int pivot = partition(ary, lm, rm); // Determine the pivot.
-        quick_sort(ary, lm, pivot - 1);     // Quicksort the elements before the pivot.
-        quick_sort(ary, pivot + 1, rm);     // Quicksort the elements after the pivot.
+        size_t pivot = partition(ary, lm, rm); // Determine the pivot.
+        quick_sort(ary, lm, pivot - 1);        // Quicksort the elements before the pivot.
+        quick_sort(ary, pivot + 1, rm);        // Quicksort the elements after the pivot.
     }
 }
