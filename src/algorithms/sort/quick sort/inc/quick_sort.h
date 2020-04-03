1e1099789308742aab4401f7ce6d6ebea478bcff
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-04-03 10:18:15 +0200

    Rename

---
+++ src/algorithms/sort/quick sort/inc/quick_sort.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdlib.h>
+
+void swap(size_t *x,size_t *y);
+size_t partition(size_t ary[], const size_t lm, const size_t rm);
+void quick_sort(size_t ary[], const size_t lm, const size_t rm);
--- src/algorithms/sort/quick sort/include/quick_sort.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void swap(int *x,int *y);
-int partition(int ary[], const int lm, const int rm);
-void quick_sort(int ary[], const int lm, const int rm);
