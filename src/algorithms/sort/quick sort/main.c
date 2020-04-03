cf4bc2b04e7dce19f3e34a6e1227604d9307ea8f
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-04-03 10:18:29 +0200

    Replace int with size_t

--- src/algorithms/sort/quick sort/main.c
+++ src/algorithms/sort/quick sort/main.c
@@ -6,8 +6,8 @@
 
 int main()
 {
-    int array[] = {7, 1, 326, 832, 225, 32323, 45, 1, 36, 43534, 34534, 2};
+    size_t array[] = {7, 1, 326, 832, 225, 32323, 45, 1, 36, 43534, 34534, 2};
     quick_sort(array, 0, LENGTH - 1);
     for (int i = 0; i < LENGTH; ++i)
-        printf("%d\n", array[i]);
+        printf("%zd\n", array[i]);
 }
