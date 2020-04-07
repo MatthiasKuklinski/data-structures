a8f4b4edf790a8e7e06425ab32543a58c13558b0
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-04-07 12:13:58 +0200

    Init header

---
+++ src/algorithms/sort/merge sort/inc/merge_sort.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(size_t ary[], size_t l_ary[], const size_t l_n, size_t r_ary[], const size_t r_n);
+void merge_sort(size_t ary[], const size_t n);
