ae0539bebaafa4258f14be1a24ddd8d4ba554cb6
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-04-30 22:55:00 +0200

    Fix comments spelling

--- stack/stack array/src/stk_ary.c
+++ stack/stack array/src/stk_ary.c
@@ -5,8 +5,8 @@
 
 stk_ary_t *stk_ary(const unsigned int capacity)
 {
-    stk_ary_t *stk_ary = malloc(sizeof(stk_ary_t));              // Allocate memory for the stack itself.
-    stk_ary->capacity = capacity;                                // Set capacity.
+    stk_ary_t *stk_ary = malloc(sizeof(stk_ary_t));              // Allocate memory for the stack structure itself.
+    stk_ary->capacity = capacity;                                // Set the maximum capacity the stack can hold.
     stk_ary->i = -1;                                             // Set the index to < 0 (empty).
     stk_ary->elements = malloc(stk_ary->capacity * sizeof(int)); // Allocate memory for the related array in accordance to the capacity.
 
@@ -25,14 +25,14 @@ int stk_ary_is_full(const stk_ary_t *stk_ary)
 
 void stk_ary_push(stk_ary_t *stk_ary, const unsigned int element)
 {
-    if (stk_ary && !stk_ary_is_full(stk_ary))      // Check if the stack has available capacity to store one more element.
+    if (stk_ary && !stk_ary_is_full(stk_ary))      // Check if the stack is defined and whether it has available capacity in order to store one more element.
         stk_ary->elements[++stk_ary->i] = element; // Increment the index and append the requested element.
 }
 
 void stk_ary_pop(stk_ary_t *stk_ary)
 {
-    if (stk_ary && !stk_ary_is_empty(stk_ary)) // Check if stk_ary is defined or if there are available elements to be removed.
-        stk_ary->i--;                          // Decrement the index(element is not removed).
+    if (stk_ary && !stk_ary_is_empty(stk_ary)) // Check if the stack is defined and whether it stores at least one element to be removed.
+        stk_ary->i--;                          // Decrement the index(do not remove/overwrite the element effectively, but prevent access to it).
 }
 
 int stk_ary_peek(stk_ary_t *stk_ary)
