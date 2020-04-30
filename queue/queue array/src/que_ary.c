895a5f06d804ec46cad92a3e968a170801b2fe62
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-04-30 22:55:38 +0200

    Add comments

--- queue/queue array/src/que_ary.c
+++ queue/queue array/src/que_ary.c
@@ -5,46 +5,46 @@
 
 que_ary_t *que_ary(const unsigned int capacity)
 {
-    que_ary_t *que_ary = malloc(sizeof(que_ary_t));
-    que_ary->capacity = capacity;
-    que_ary->length = 0;
-    que_ary->front = que_ary->rear = -1;
-    que_ary->elements = malloc(que_ary->capacity * sizeof(int));
+    que_ary_t *que_ary = malloc(sizeof(que_ary_t));              // Allocate memory for the queue structure itself.
+    que_ary->capacity = capacity;                                // Set the maximum capacity the queue can hold.
+    que_ary->length = 0;                                         // Initialize the length to 0.
+    que_ary->front = que_ary->rear = -1;                         // Set the lower and upper bound indice to -1(represent an empty queue).
+    que_ary->elements = malloc(que_ary->capacity * sizeof(int)); // Allocate memory for the related array in accordance to the capacity.
 
     return que_ary;
 }
 
 int que_ary_is_empty(const que_ary_t *que_ary)
 {
-    return que_ary->length < 1;
+    return que_ary->length < 1; // Check if the queue length is smaller than one and return the result.
 }
 
 int que_ary_is_full(const que_ary_t *que_ary)
 {
-    return que_ary->rear == que_ary->capacity -1;
+    return que_ary->rear == que_ary->capacity - 1; // Check if the upper bound index is of the same size as the capacity substracted by one and return the result.
 }
 
-void que_ary_enqueue(que_ary_t *que_ary, int item)
+void que_ary_enqueue(que_ary_t *que_ary, int element)
 {
-    if (que_ary && !que_ary_is_full(que_ary))
+    if (que_ary && !que_ary_is_full(que_ary)) // Check if the queue is defined and whether it has available capacity in order to store one more element.
     {
-        if (que_ary->front == -1)
-            que_ary->front++;
+        if (que_ary->front == -1) // Check if the queue is enqueued for the first time(doesn't hold any elements).
+            que_ary->front++;     // Increment the lower bound.
 
-        que_ary->elements[++que_ary->rear] = item;
-        que_ary->length++;
+        que_ary->elements[++que_ary->rear] = element; // Increment the upper bound and store the element afterwards.
+        que_ary->length++;                            // Increment the length.
     }
 }
 
 void que_ary_dequeue(que_ary_t *que_ary)
 {
-    if (que_ary && !que_ary_is_empty(que_ary))
+    if (que_ary && !que_ary_is_empty(que_ary)) // Check if the queue is defined and whether it stores at least one element to be removed.
     {
-        que_ary->front++;
-        que_ary->length--;
+        que_ary->front++;  // Increment the lower bound(do not remove/overwrite the element effectively, but prevent access to it).
+        que_ary->length--; // Decrement the length.
 
-        if (que_ary->front > que_ary->rear)
-            que_ary->front = que_ary->rear = -1;
+        if (que_ary->front > que_ary->rear)      // Check if the lower bound overtook the upper bound(signal an empty queue).
+            que_ary->front = que_ary->rear = -1; // Set the lower and upper bound indice to -1(represent an empty queue).
     }
 }
 
