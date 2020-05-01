1d631cba2eeadd42d65f113a6c305093b4083ebe
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-05-01 21:24:40 +0200

    Add status codes

--- stack/stack array/include/stk_ary.h
+++ stack/stack array/include/stk_ary.h
@@ -1,16 +1,26 @@
 #pragma once
 
+typedef enum status_code
+{
+    success = 0,
+    stk_ary_ptr_is_null = -1,
+    insufficient_heap_mem = -2,
+    stk_ary_is_full = -3,
+    stk_ary_is_empty = -4
+} status_code_t;
+
 typedef struct stk_ary
 {
-    int i;                 // Store the index of the top element on the stack.
-    unsigned int capacity; // Store the maximum capacity of elements the stack can hold.
-    int *elements;         // Point to the first element of the stack(element at the bottom of the stack).
+    int top;       // Store the index of the top element on the stack.
+    int capacity;  // Store the maximum capacity of elements the stack can hold.
+    int *elements; // Point to the first element of the stack(element at the bottom of the stack).
 } stk_ary_t;
 
-stk_ary_t *stk_ary(const unsigned int capacity);                   // Construct the stack structure.
-int stk_ary_is_empty(const stk_ary_t *stk_ary);                    // Check if the stack is empty.
-int stk_ary_is_full(const stk_ary_t *stk_ary);                     // Check if the stack is full.
-void stk_ary_push(stk_ary_t *stk_ary, const unsigned int element); // Add an element onto the top of the stack.
-void stk_ary_pop(stk_ary_t *stk_ary);                              // Remove an element of the top of the stack.
-int stk_ary_peek(stk_ary_t *stk_ary);                              // Get the element on the top of the stack.
-void stk_ary_delete(stk_ary_t **stk_ary);                          // Free the allocated memory.
+stk_ary_t *stk_ary(const int capacity, status_code_t *status_code);                   // Construct the stack structure.
+int stk_ary_is_empty(const stk_ary_t *stk_ary, status_code_t *status_code);           // Check if the stack is empty.
+int stk_ary_is_full(const stk_ary_t *stk_ary, status_code_t *status_code);            // Check if the stack is full.
+void stk_ary_push(stk_ary_t *stk_ary, const int element, status_code_t *status_code); // Add an element onto the top of the stack.
+void stk_ary_pop(stk_ary_t *stk_ary, status_code_t *status_code);                     // Remove an element of the top of the stack.
+int stk_ary_peek(stk_ary_t *stk_ary, status_code_t *status_code);                     // Get the element on the top of the stack.
+// void stk_ary_traverse(stk_ary_t *stk_ary, const sll_callback cb);                     // Traverse through the stack elements array and call the passed in function
+void stk_ary_delete(stk_ary_t **stk_ary, status_code_t *status_code);                 // Free the allocated memory.
--- stack/stack array/src/stk_ary.c
+++ stack/stack array/src/stk_ary.c
@@ -3,49 +3,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-stk_ary_t *stk_ary(const unsigned int capacity)
+stk_ary_t *stk_ary(const int capacity, status_code_t *status_code)
 {
-    stk_ary_t *stk_ary = malloc(sizeof(stk_ary_t));              // Allocate memory for the stack structure itself.
-    stk_ary->capacity = capacity;                                // Set the maximum capacity the stack can hold.
-    stk_ary->i = -1;                                             // Set the index to < 0 (empty).
-    stk_ary->elements = malloc(stk_ary->capacity * sizeof(int)); // Allocate memory for the related array in accordance to the capacity.
+    stk_ary_t *stk_ary = NULL;                  // Initialize a null pointer for the stack structure itself.
+    if (!(stk_ary = malloc(sizeof(stk_ary_t)))) // Try to allocate sufficient memory on the heap.
+    {
+        *status_code = insufficient_heap_mem; // Set the correspoding status code.
+        return stk_ary;                       // Stk_ary will be null, since there is no sufficient memory on the heap.
+    }
 
+    stk_ary->capacity = capacity; // Set the capacity the stack can hold.
+
+    stk_ary->elements = NULL;                                           // Initialize a null pointer for the elements array.
+    if (!(stk_ary->elements = malloc(stk_ary->capacity * sizeof(int)))) // Try to allocate sufficient memory on the heap.
+    {
+        free(stk_ary);                        // Free the allocated memory for the stack structure itself.
+        *status_code = insufficient_heap_mem; // Set the correspoding status code.
+        return NULL;
+    }
+
+    stk_ary->top = -1; // Set the top index to < 0 (empty).
+
+    *status_code = success; // Set the correspoding status code.
     return stk_ary;
 }
 
-int stk_ary_is_empty(const stk_ary_t *stk_ary)
+int stk_ary_is_empty(const stk_ary_t *stk_ary, status_code_t *status_code)
 {
-    return stk_ary->i < 0; // Check if the stk_ary index is smaller than 0 and return the result.
-}
-
-int stk_ary_is_full(const stk_ary_t *stk_ary)
-{
-    return stk_ary->i == stk_ary->capacity - 1; // Check if the index is of the same size as the capacity -1 and return the result.
-}
-
-void stk_ary_push(stk_ary_t *stk_ary, const unsigned int element)
-{
-    if (stk_ary && !stk_ary_is_full(stk_ary))      // Check if the stack is defined and whether it has available capacity in order to store one more element.
-        stk_ary->elements[++stk_ary->i] = element; // Increment the index and append the requested element.
-}
-
-void stk_ary_pop(stk_ary_t *stk_ary)
-{
-    if (stk_ary && !stk_ary_is_empty(stk_ary)) // Check if the stack is defined and whether it stores at least one element to be removed.
-        stk_ary->i--;                          // Decrement the index(do not remove/overwrite the element effectively, but prevent access to it).
-}
-
-int stk_ary_peek(stk_ary_t *stk_ary)
-{
-    return stk_ary->elements[stk_ary->i]; // Access and return the element at the top of the stack.
-}
-
-void stk_ary_delete(stk_ary_t **stk_ary)
-{
-    if (!*stk_ary) // Check if stk_ary is defined.
+    if (!stk_ary) // Check if the stack pointer is defined.
+    {
+        *status_code = stk_ary_ptr_is_null; // Set the correspoding status code.
         return;
+    }
+
+    *status_code = success;  // Set the correspoding status code.
+    return stk_ary->top < 0; // Check if the stack top index is smaller than 0 (empty) and return the result.
+}
+
+int stk_ary_is_full(const stk_ary_t *stk_ary, status_code_t *status_code)
+{
+    if (!stk_ary) // Check if the stack pointer is defined.
+    {
+        *status_code = stk_ary_ptr_is_null; // Set the correspoding status code.
+        return;
+    }
+
+    *status_code = success;                       // Set the correspoding status code.
+    return stk_ary->top == stk_ary->capacity - 1; // Check if the stack top index is of the same size as the capacity -1 and return the result.
+}
+
+void stk_ary_push(stk_ary_t *stk_ary, const int element, status_code_t *status_code)
+{
+    if (!stk_ary) // Check if the stack pointer is defined.
+    {
+        *status_code = stk_ary_ptr_is_null; // Set the correspoding status code.
+        return;
+    }
+
+    if (stk_ary_is_full(stk_ary, status_code)) // Check if the stack array has available capacity in order to store one more element.
+    {
+        *status_code = stk_ary_is_full; // Set the correspoding status code.
+        return;
+    }
+
+    stk_ary->elements[++stk_ary->top] = element; // Increment the stack top index and append the requested element afterwards.
+    *status_code = success;                      // Set the correspoding status code.
+}
+
+void stk_ary_pop(stk_ary_t *stk_ary, status_code_t *status_code)
+{
+    if (!stk_ary) // Check if the stack pointer is defined.
+    {
+        *status_code = stk_ary_ptr_is_null; // Set the correspoding status code.
+        return;
+    }
+
+    if (stk_ary_is_empty(stk_ary, status_code)) // Check if the stack array stores at least one removable element.
+    {
+        *status_code = stk_ary_is_empty; // Set the correspoding status code.
+        return;
+    }
+
+    stk_ary->top--;         // Decrement the stack top index index(do not remove nor overwrite the element, only prevent accessing it via the stack interface).
+    *status_code = success; // Set the correspoding status code.
+}
+
+int stk_ary_peek(stk_ary_t *stk_ary, status_code_t *status_code)
+{
+    if (!stk_ary) // Check if the stack pointer is defined.
+    {
+        status_code = stk_ary_ptr_is_null; // Set the correspoding status code.
+        return;
+    }
+
+    if (stk_ary_is_empty(stk_ary, status_code)) // Check if the stack array stores at least one removable element.
+    {
+        status_code = stk_ary_is_empty; // Set the correspoding status code.
+        return;
+    }
+
+    *status_code = success;                 // Set the correspoding status code.
+    return stk_ary->elements[stk_ary->top]; // Access and return the element at the top of the stack.
+}
+
+void stk_ary_delete(stk_ary_t **stk_ary, status_code_t *status_code)
+{
+    if (!stk_ary) // Check if the stack pointer is defined.
+    {
+        status_code = stk_ary_ptr_is_null; // Set the correspoding status code.
+        return;
+    }
 
     free((*stk_ary)->elements); // Deallocate the elements array.
     free(*stk_ary);             // Deallocate the stack.
     *stk_ary = NULL;            // Avoid a dangling pointer.
+    *status_code = success;     // Set the correspoding status code.
 }
