c2fb3ec987ed79039af08557835b90e6416dc46a
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-05-08 17:25:16 +0200

    Add status_code

--- linked list/singly linked list/src/sll_node.c
+++ linked list/singly linked list/src/sll_node.c
@@ -1,15 +1,21 @@
-#include <sll_node.h>
+#include "sll_node.h"
+#include "sll_status_codes.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-sll_node_t *sll_node(const int element, sll_node_t *next)
+sll_node_t *sll_node(const int element, sll_node_t *next, status_code_t *status_code)
 {
     sll_node_t *sll_node = NULL;                  // Initialize a null pointer for the node structure itself.
     if (!(sll_node = malloc(sizeof(sll_node_t)))) // Try to allocate sufficient memory on the heap.
-        return NULL;
+    {
+        *status_code = insufficient_heap_mem; // Set the correspoding status code.
+        return sll_node;                      // Sll_node will be null, since there is no sufficient memory on the heap.
+    }
 
     sll_node->element = element; // Initialize the nodes element with the passed in element value.
     sll_node->next = next;       // Store the memory address of the the successing node.
+
+    *status_code = success; // Set the correspoding status code.
     return sll_node;
 }
