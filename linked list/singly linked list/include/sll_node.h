4a6ed56cd487caaa54ee4db0e7685bdc802b3319
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-05-09 17:28:57 +0200

    Fix comment formating

--- linked list/singly linked list/include/sll_node.h
+++ linked list/singly linked list/include/sll_node.h
@@ -4,7 +4,7 @@
 
 typedef struct sll_node
 {
-    int element;       // Store the value of the node.
+    int element;           // Store the value of the node.
     struct sll_node *next; // Point to the successing node.
 } sll_node_t;
 
