5d83ccf1a6fb00feeba7aeb765ea5723e8ecc431
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-05-11 15:04:59 +0200

    Fix memory allocation

--- hash table/hash table singly linked list (Chaining)/src/ht_node_sll.c
+++ hash table/hash table singly linked list (Chaining)/src/ht_node_sll.c
@@ -1,16 +1,16 @@
-#include "ht_node_sll.h";
+#include "ht_node_sll.h"
+
+#include <stdio.h>
 
 ht_node_sll_t *ht_node_sll(const char *key, const char *element, status_code_t *status_code)
 {
     ht_node_sll_t *ht_node_sll = NULL; // Initialize a null pointer for the node structure itself.
-    ht_node_sll->key = NULL;            // Initialize a null pointer for the node key (string).
-    ht_node_sll->element = NULL;        // Initialize a null pointer for the node element (string).
 
     // Try to allocate sufficient memory on the heap.
-    if (!(ht_node_sll = malloc(strlen(key) + 1)) || !(ht_node_sll = malloc(strlen(key) + 1)) || !(ht_node_sll = malloc(sizeof(ht_node_sll_t))))
+    if (!(ht_node_sll = malloc(sizeof(ht_node_sll_t))) || !(ht_node_sll->key = malloc(strlen(key) + 1)) || !(ht_node_sll->element = malloc(strlen(element) + 1)))
     {
         *status_code = insufficient_heap_mem; // Set the correspoding status code.
-        return ht_node_sll;                  // ht_node_sll will be null, since there was no sufficient memory on the heap to be allocated for it.
+        return ht_node_sll;                   // ht_node_sll will be null, since there was no sufficient memory on the heap to be allocated for it.
     }
 
     strcpy(ht_node_sll->key, key);         // Copy the key argument into the node key.
