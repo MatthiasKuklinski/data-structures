23bd5e32241de9c7296ae9871b6d4079874908e7
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-05-13 11:02:38 +0200

    Rename

--- queue/circular queue array/include/cir_que_ary.h
+++ queue/circular queue array/include/cir_que_ary.h
@@ -1,26 +1,18 @@
 #pragma once
 
-typedef enum status_code
-{
-    success = 0,
-    cir_que_ary_ptr_is_null = -1,
-    insufficient_heap_mem = -2,
-    cir_que_ary_full = -3,
-    cir_que_ary_empty = -4
-} status_code_t;
+#include "cir_que_ary_status.h"
 
 typedef struct cir_que_ary
 {
-    int capacity;    // Store the maximum capacity of the queue.
-    int front, rear; // Store the lower(front) and the upper(rear) bound index respectively.
-    int *elements;   // Point to the first element of the queue(element at the front of the queue).
+    int capacity, front, rear;
+    int *elements;
 } cir_que_ary_t;
 
-cir_que_ary_t *cir_que_ary(const int capacity, status_code_t *status_code);                           // Construct the queue structure.
-int cir_que_ary_is_empty(const cir_que_ary_t *cir_que_ary, status_code_t *status_code);               // Check if the queue is empty.
-int cir_que_ary_is_full(const cir_que_ary_t *cir_que_ary, status_code_t *status_code);                // Check if the queue is full.
-void cir_que_ary_enqueue(cir_que_ary_t *cir_que_ary, int element, status_code_t *status_code);        // Add an element at the end of the queue.
-void cir_que_ary_dequeue(cir_que_ary_t *cir_que_ary, status_code_t *status_code);                     // Remove an element from the front of the queue.
-int cir_que_ary_peek(const cir_que_ary_t *cir_que_ary, status_code_t *status_code);                   // Get the element at the front of the queue.
-void cir_que_ary_traverse(cir_que_ary_t *cir_que_ary, void (*fp)(int *), status_code_t *status_code); // Traverse through the queue elements array and call the passed in function
-void cir_que_ary_delete(cir_que_ary_t **cir_que_ary, status_code_t *status_code);                     // Free the allocated memory.
+cir_que_ary_t *cir_que_ary_alloc(const int capacity, status_t *status);
+void cir_que_ary_dealloc(cir_que_ary_t **cir_que_ary, status_t *status);
+void cir_que_ary_enqueue(cir_que_ary_t *cir_que_ary, int element, status_t *status);
+void cir_que_ary_dequeue(cir_que_ary_t *cir_que_ary, status_t *status);
+int cir_que_ary_peek(const cir_que_ary_t *cir_que_ary, status_t *status);
+void cir_que_ary_traverse(const cir_que_ary_t *cir_que_ary, void (*fp)(int *), status_t *status);
+unsigned short cir_que_ary_is_empty(const cir_que_ary_t *cir_que_ary, status_t *status);
+unsigned short cir_que_ary_is_full(const cir_que_ary_t *cir_que_ary, status_t *status);
