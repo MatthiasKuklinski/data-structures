9570d92fbd4ee3e976dcd11cfb5d408e72985088
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-05-14 10:47:59 +0200

    Fix types

--- stack/stack array/include/stk_ary.h
+++ stack/stack array/include/stk_ary.h
@@ -5,14 +5,14 @@
 typedef struct stk_ary
 {
     long top, capacity;
-    size_t *elements;
+    int *elements;
 } stk_ary_t;
 
 stk_ary_t *stk_ary_alloc(const long capacity);
 void stk_ary_dealloc(stk_ary_t **stk_ary);
-void stk_ary_push(stk_ary_t *stk_ary, const size_t element);
+void stk_ary_push(stk_ary_t *stk_ary, const int element);
 void stk_ary_pop(stk_ary_t *stk_ary);
-size_t stk_ary_peek(const stk_ary_t *stk_ary);
-void stk_ary_traverse(stk_ary_t *stk_ary, void (*fp)(size_t **element));
+int stk_ary_peek(const stk_ary_t *stk_ary);
+void stk_ary_traverse(stk_ary_t *stk_ary, void (*fp)(int **element));
 unsigned short stk_ary_is_empty(const stk_ary_t *stk_ary);
 unsigned short stk_ary_is_full(const stk_ary_t *stk_ary);
--- stack/stack array/src/stk_ary.c
+++ stack/stack array/src/stk_ary.c
@@ -1,5 +1,5 @@
 #include "stk_ary.h"
-#include <stdio.h>
+
 stk_ary_t *stk_ary_alloc(const long capacity)
 {
     stk_ary_t *stk_ary = malloc(sizeof(stk_ary_t));
@@ -21,7 +21,7 @@ unsigned short stk_ary_is_full(const stk_ary_t *stk_ary)
     return stk_ary->top == stk_ary->capacity - 1;
 }
 
-void stk_ary_push(stk_ary_t *stk_ary, const size_t element)
+void stk_ary_push(stk_ary_t *stk_ary, const int element)
 {
     if (!stk_ary_is_full(stk_ary))
         stk_ary->elements[++stk_ary->top] = element;
@@ -33,19 +33,15 @@ void stk_ary_pop(stk_ary_t *stk_ary)
         stk_ary->top--;
 }
 
-size_t stk_ary_peek(const stk_ary_t *stk_ary)
+int stk_ary_peek(const stk_ary_t *stk_ary)
 {
-    if (!stk_ary_is_empty(stk_ary))
-        return stk_ary->elements[stk_ary->top];
-    else
-    {
-        return -1;
-    }
+    return stk_ary->elements[stk_ary->top];
 }
 
-void stk_ary_traverse(stk_ary_t *stk_ary, void (*fp)(size_t **element))
+void stk_ary_traverse(stk_ary_t *stk_ary, void (*fp)(int **element))
 {
-    size_t *temp_ptr_element; // Store the address of an element (necessary, since it is a rvalue).
+    // Temporarily store the address of an element (necessary, since it is a rvalue).
+    int *temp_ptr_element;
     for (long i = stk_ary->top; i > -1; --i)
     {
         temp_ptr_element = &stk_ary->elements[i];
